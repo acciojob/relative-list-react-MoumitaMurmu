3d341dd23d5e191eb7d8418b6357ea9167453c12
Moumita Murmu <69195751+MoumitaMurmu@users.noreply.github.com>
2023-01-08 01:50:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+    const arr=["A","B","C"]
+  return (
+    <div>
+        <ol id='relativeList'>
+        {arr.map((el,i)=>
+            <li id={`relativeListItem${i+1}`} key={`relativeListItem${i+1}`}>{el}</li>
+        )}
+        </ol>
+    </div>
+  )
+}
+
+export default App
